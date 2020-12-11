5253a91980f044f19d905babe4b1f393b404a6ec
Evelina Wenner <evelina.nilssonvenner@medieinstitutet.se>
2020-12-11 14:35:48 +0100

    ändringar i main

--- js/main.js
+++ js/main.js
@@ -12,18 +12,18 @@ class Product {
 
 $(function() {
     
-   var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+//    var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
 //img.attr('src', responseObject.imgurl);
 //img.appendTo('#imagediv');
 
 
 
-    let p1 = new Product("elise", 'url("/css/images/gran1.png")', "Elise", "Toppengran verkligen", "500kr");
-    let p2 = new Product("kristoffer", 'url("/css/images/gran2.png")', "Kristoffer", "Toppengran verkligen", "500kr");
-    let p3 = new Product("ivan", 'url("/css/images/gran3.png")', "Ivan", "Toppengran verkligen", "500kr");
-    let p4 = new Product("lydia", 'url("/css/images/gran4.png")', "Lydia", "Toppengran verkligen", "500kr");
-    let p5 = new Product("vera", 'url("/css/images/gran5.png")', "Vera", "Toppengran verkligen", "500kr");
-    let p6 = new Product("jan", 'url(/css/images/gran6.png)', "Jan", "Toppengran verkligen", "500kr");
+    let p1 = new Product("elise", '../images/gran1.jpg', "Elise", "Toppengran verkligen", "500");
+    let p2 = new Product("kristoffer", '../images/gran2.png', "Kristoffer", "Toppengran verkligen", "500");
+    let p3 = new Product("ivan", '../images/gran3.png', "Ivan", "Toppengran verkligen", "500");
+    let p4 = new Product("lydia", '../images/gran4.png', "Lydia", "Toppengran verkligen", "500");
+    let p5 = new Product("vera", '../images/gran5.png', "Vera", "Toppengran verkligen", "500");
+    let p6 = new Product("jan", '../images/gran6.png', "Jan", "Toppengran verkligen", "500");
 
     let products = [p1, p2, p3, p4, p5, p6];
 
@@ -33,10 +33,11 @@ $(function() {
     let container = $("#productlist");
     let listitem = $("<li>")
 
-   // img.attr('src', product.image).appendTo(container);
-   $("<div>").html(product.image).addClass("image").appendTo(listitem);
+    
+   $("<img>").addClass("image").attr('src', product.image).appendTo(listitem);
+   $("<p>").html(product.price + "kr").addClass("price").appendTo(listitem);
     $("<p>").html(product.name).addClass("name").appendTo(listitem);
-    $("<p>").html(product.price).addClass("price").appendTo(listitem);
+
 
     listitem.appendTo(container);
     container.appendTo($(".main"));
