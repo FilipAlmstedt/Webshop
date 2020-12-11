c1b2bf44317a71efdef564734f74bee5482d972d
FilipAlmstedt <filip.almstedt@almxer.se>
2020-12-11 12:44:03 +0100

    added comments,variables and mixins

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(function() {
-    $(".toggle").on("click", function() {
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-        } 
-        else {
-            $(".item").addClass("active");
-        }
-    });
+    $(".toggle").on("click",openMobileNavbar);
 });
+
+/*Aktiverar klassen active i css som gör att navbar visas vertikalt i mobilt läge*/ 
+function openMobileNavbar(){
+    if ($(".item").hasClass("active")) {
+        $(".item").removeClass("active");
+    } 
+    else {
+        $(".item").addClass("active");
+    }
+}
